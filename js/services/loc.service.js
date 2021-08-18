f32e983771f133d15fa6fe76c38eb23e80ae3333
Yotke <yotam.elia@gmail.com>
2021-08-18 15:35:46 +0300

    changed the local service

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,12 +1,48 @@
 export const locService = {
-    getLocs
+    getLocs,
+    setLocation
+}
+
+const locs = [
+    {
+        id: 1,
+        name: 'Greatplace',
+        lat: 32.047104, lng: 34.832384,
+        weather: '',
+        createdAt: Date.now(),
+        updateAt: Date.now()
+    },
+    { id: 2, name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+]
+
+
+
+function setLocation(id, name, lat, lng, weather = '') {
+    if (findLocById(id) === -1) {
+        console.log('Existing Location');
+        return;
+    }
+    
+    locs.push({
+        id,
+        name,
+        lat,
+        lng,
+        weather,
+        createdAt: Date.now(),
+        updateAt: Date.now()
+    });
+    console.log('set location successful');
 }
 
 
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
+
+function findLocById(id) {
+    return locs.find(loc => {
+        return loc[id] === id;
+    });
+
+}
 
 function getLocs() {
     return new Promise((resolve, reject) => {
